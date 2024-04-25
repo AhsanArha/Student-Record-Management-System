dd374d83936adc629fc7c22aa74bc9e473c3262f
AhsanArha <141950111+AhsanArha@users.noreply.github.com>
2024-04-25 22:10:59 +0530

    Add files via upload

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-4">
+      <h6>All Rights Reserved @ ZHCET </h6>
+    </div>
+  );
+};
+
+export default Footer;
